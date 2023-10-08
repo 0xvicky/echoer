c74b52039348fbc64c77bf40f821e2971960e473
vicky <tyagivivek528@gmail.com>
2023-10-08 23:10:25 +0530

    backend setup done and started work on Home component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className='text-green-400'>
-        <h1 className='text-4xl'>Welcome to the Next.js template</h1>
+        <h1 className='text-4xl'>Echoer is headphone website</h1>
       </div>
     </>
   );
